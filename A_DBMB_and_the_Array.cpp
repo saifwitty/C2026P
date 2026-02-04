7ed32d5952bcfee30cc09c2ecb51314ecdc401be
saifwitty <saifwitty@gmail.com>
2026-02-05 01:12:56 +0600

    updated

--- A_DBMB_and_the_Array.cpp
+++ A_DBMB_and_the_Array.cpp
@@ -13,8 +13,36 @@ int lcm(int a,int b) {
     return (a * b) / (__gcd(a,b));
 }
 void saif(){
-    
+    int n,s,x;
+    cin >>n>>s>>x;
+    vector<int>v(n);
+    ll sum=0;
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+        sum+=v[i];
+    }
+    if(sum==s){
+        yes
+        return;
+    }
+    else if(s<sum){
+        no
+        return;
+        
+    }
+    else {
+        ll dif=s-sum;
+        if(dif%x){
+            no
+            return;
+        }
+        else{
+            yes
+            return;
+        }
+    }
    }
+
 signed main(){
     fast_cin();
     tt saif();
