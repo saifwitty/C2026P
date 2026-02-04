abf3e9ff46ea27d1c5d3124bb580183dcceb7f96
saifwitty <saifwitty@gmail.com>
2026-02-05 01:03:25 +0600

    a

---
+++ A_DBMB_and_the_Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<ll> vi;
+#define all(a) a.begin(),a.end()
+#define print(a) for(auto i : a) cout << i << ' '
+#define pb push_back
+#define yes cout<<'Y'<<'e'<<'s'<<'\n';
+#define no cout<<'N'<<'o'<<'\n';
+#define tt int t;cin>>t; for(int tt=1;tt<=t;tt++)
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int lcm(int a,int b) {
+    return (a * b) / (__gcd(a,b));
+}
+void saif(){
+    
+   }
+signed main(){
+    fast_cin();
+    tt saif();
+  }
